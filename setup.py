782923d6e26135d2a26f6fba698fee415bd5a84a
DingWB <ding.wu.bin.gm@gmail.com>
2024-03-12 22:42:24 -0700

    update readme

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ from pathlib import Path
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-# long_description="upload, download, delete and get information from figshare"
 
 setup(
 	name="pyfigshare",
