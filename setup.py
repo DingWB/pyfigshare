1efbed53f5437946ad524e860710b9b31d78da24
DingWB <ding.wu.bin.gm@gmail.com>
2024-03-12 22:42:00 -0700

    update readme

--- setup.py
+++ setup.py
@@ -8,25 +8,25 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-# long_description = (this_directory / "README.md").read_text()
-long_description="upload, download, delete and get information from figshare"
+long_description = (this_directory / "README.md").read_text()
+# long_description="upload, download, delete and get information from figshare"
 
 setup(
-    name="pyfigshare",
+	name="pyfigshare",
 	use_scm_version=True,  # {'version_scheme': 'python-simplified-semver',"local_scheme": "no-local-version"},
 	setup_requires=['setuptools_scm'],
-    description="A Python package to interact with figshare",
-    author="Wubin Ding",
-    author_email="ding.wu.bin.gm@gmail.com",
-    url="https://github.com/DingWB/pyfigshare",
-    # packages=['pyfigshare'],  # pyfigshare
-	# package_dir={'': 'pyfigshare'},
+	description="A Python package to interact with figshare",
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	author="Wubin Ding",
+	author_email="ding.wu.bin.gm@gmail.com",
+	url="https://github.com/DingWB/pyfigshare",
 	license='MIT',
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-    ],
+	classifiers=[
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.7",
+	],
 	packages=find_packages(exclude=('docs',)),
 	install_requires=['fire','pandas','requests'],
 	include_package_data=True,
