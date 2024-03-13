3761bd055e7fe232088e047dfe462de05f5fdf2c
DingWB <ding.wu.bin.gm@gmail.com>
2024-03-12 20:23:54 -0700

    update setup

--- pyfigshare/__init__.py
+++ pyfigshare/__init__.py
@@ -6,12 +6,12 @@ from ._version import version as __version__
 def main():
     fire.core.Display = lambda lines, out: print(*lines, file=out)
     fire.Fire(
-        {
-            "upload": upload,
-            'Figshare': Figshare,
+		{
+			"upload": upload,
+			'Figshare': Figshare,
 			'download':download,
 			'get_filenames':get_filenames,
-        }
+		}
 	)
 
 if __name__=="__main__":
