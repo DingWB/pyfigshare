ebfb4a33a78b704e9ffd276ed7a353d8f3ba77e2
DingWB <ding.wu.bin.gm@gmail.com>
2024-06-29 14:27:03 -0700

    support parallelly uploading

--- pyfigshare/__init__.py
+++ pyfigshare/__init__.py
@@ -1,7 +1,7 @@
 import os,sys
 import fire
 from .figshare import (
-	upload,show_files,download,get_account_info,
+	search_articles,upload,show_files,download,get_account_info,
 	publish,get_file_details,list_article_versions,
 	delete_articles_with_title,delete_all_files,
 	delete_file,delete_article,list_articles
