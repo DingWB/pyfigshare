157a6f8c6259c7c4ff9518f23c1e367a27110ea1
DingWB <ding.wu.bin.gm@gmail.com>
2024-06-29 14:39:03 -0700

    support parallelly uploading

--- pyfigshare/__init__.py
+++ pyfigshare/__init__.py
@@ -10,7 +10,15 @@ from ._version import version as __version__
 
 def main():
     fire.core.Display = lambda lines, out: print(*lines, file=out)
-    fire.Fire(serialize=lambda x:print(x) if not x is None else print(""))
+    fire.Fire({
+		'search_articles':search_articles,
+		'upload':upload,'show_files':show_files,'download':download,
+		'get_file_details':get_file_details,'list_article_versions':list_article_versions,
+		'delete_articles_with_title':delete_articles_with_title,
+		'delete_all_files':delete_all_files,'delete_file':delete_file,
+		'delete_article':delete_article,'list_articles':list_articles,
+	},serialize=lambda x:print(x) if not x is None else print("")
+	)
 
 if __name__=="__main__":
     main()
